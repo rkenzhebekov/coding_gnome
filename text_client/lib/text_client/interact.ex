0955014fa75a1041dc8a4bcafae79b8391e39dc5
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2018-02-19 21:53:34 +0600

    Distributed Text Clients

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,9 +1,12 @@
 defmodule TextClient.Interact do
 
+  @hangman_server :hangman@kenrus
+
   alias TextClient.{State, Player}
 
+
   def start() do
-    Hangman.new_game()
+    new_game()
     |> setup_state()
     |> Player.play()
   end
@@ -13,6 +16,11 @@ defmodule TextClient.Interact do
       game_service: game,
       tally: Hangman.tally(game)
     }
+  end
+
+  defp new_game() do
+    Node.connect(@hangman_server)
+    :rpc.call(@hangman_server, Hangman, :new_game, [])
   end
 end
 
