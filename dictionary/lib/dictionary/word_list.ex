18bbc105c945eef3004f5c843eb1d73cfc5ebf6e
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2018-02-24 21:01:33 +0600

    Load external resource as module attribute
    This allows to carry words list when dictionary app is deployed in
    different location

--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -2,6 +2,8 @@ defmodule Dictionary.WordList do
 
   @me __MODULE__
 
+  @external_resource File.read!(Path.expand("../../assets/words.txt", __DIR__))
+
   def start_link() do
     Agent.start_link(&word_list/0, name: @me)
   end
@@ -11,9 +13,8 @@ defmodule Dictionary.WordList do
   end
 
   def word_list() do
-    "../../assets/words.txt"
-    |> Path.expand(__DIR__)
-    |> File.read!()
+    [ content |_ ] = @external_resource
+    content
     |> String.split(~r/\n/)
   end
 end
