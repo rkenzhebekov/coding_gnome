0b21d088b4d112a03e17f9eb47dbec7aec942b39
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2018-03-04 14:05:22 +0600

    Setup elm

--- socket_gallows/assets/brunch-config.js
+++ socket_gallows/assets/brunch-config.js
@@ -37,7 +37,7 @@ exports.config = {
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
+    watched: ["static", "css", "js", "vendor", "elm"],
     // Where to compile files to
     public: "../priv/static"
   },
@@ -46,7 +46,17 @@ exports.config = {
   plugins: {
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/vendor/]
+      ignore: [
+        /vendor/,
+        "js/elm.js"
+      ]
+    },
+    elmBrunch: {
+      elmFolder: "elm",
+      mainModules: ["src/Main.elm"],
+      outputFolder: "../js",
+      outputFile: "elm.js",
+      makeParameters: ["--warn", "--debug"]
     }
   },
 
--- socket_gallows/assets/js/app.js
+++ socket_gallows/assets/js/app.js
@@ -3,3 +3,9 @@ window.Vue = require("vue/dist/vue.js")
 import "phoenix_html"
 
 import "./hangman_app"
+
+import Elm from "./elm"
+
+const elmDiv = document.getElementById("elm-main")
+
+Elm.Main.embed(elmDiv)
